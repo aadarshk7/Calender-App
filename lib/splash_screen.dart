88aceb5d98e2f81a15e07bae74a9b06971521e13
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-07-11 22:18:01 +0545

    Calender app

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/calendar');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('images/assets/calender.jpg'),
+            image: AssetImage('assets/images/meroaichatbot.jpg'),
             fit: BoxFit.cover,
           ),
         ),
