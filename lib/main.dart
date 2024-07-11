c93dfc0317a2a0ba11f4386363494ed8c8ed2221
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-07-12 00:41:26 +0545

    Calender app

--- lib/main.dart
+++ lib/main.dart
@@ -81,94 +81,115 @@ class _CalendarHomePageState extends State<CalendarHomePage> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            TableCalendar(
-              firstDay: DateTime.utc(2010, 10, 16),
-              lastDay: DateTime.utc(2030, 3, 14),
-              focusedDay: _focusedDay,
-              calendarFormat: _calendarFormat,
-              selectedDayPredicate: (day) {
-                return isSameDay(_selectedDay, day);
-              },
-              onDaySelected: (selectedDay, focusedDay) {
-                setState(() {
-                  _selectedDay = selectedDay;
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TableCalendar(
+                firstDay: DateTime.utc(2010, 10, 16),
+                lastDay: DateTime.utc(2030, 3, 14),
+                focusedDay: _focusedDay,
+                calendarFormat: _calendarFormat,
+                selectedDayPredicate: (day) {
+                  return isSameDay(_selectedDay, day);
+                },
+                onDaySelected: (selectedDay, focusedDay) {
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+                  });
+                },
+                onFormatChanged: (format) {
+                  if (_calendarFormat != format) {
+                    setState(() {
+                      _calendarFormat = format;
+                    });
+                  }
+                },
+                onPageChanged: (focusedDay) {
                   _focusedDay = focusedDay;
-                });
-              },
-              onFormatChanged: (format) {
-                if (_calendarFormat != format) {
-                  setState(() {
-                    _calendarFormat = format;
-                  });
-                }
-              },
-              onPageChanged: (focusedDay) {
-                _focusedDay = focusedDay;
-              },
-              calendarStyle: CalendarStyle(
-                todayDecoration: BoxDecoration(
-                  color: Colors.blue,
-                  shape: BoxShape.circle,
+                },
+                calendarStyle: CalendarStyle(
+                  todayDecoration: BoxDecoration(
+                    color: Colors.blue,
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.white, width: 2),
+                  ),
+                  selectedDecoration: BoxDecoration(
+                    color: Colors.lightBlueAccent,
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.white, width: 2),
+                  ),
+                  defaultTextStyle: TextStyle(
+                    color: widget.isDarkMode ? Colors.white : Colors.black,
+                  ),
+                  weekendTextStyle: TextStyle(
+                    color: widget.isDarkMode ? Colors.white : Colors.black,
+                  ),
+                  outsideTextStyle: TextStyle(
+                    color: widget.isDarkMode ? Colors.white : Colors.black,
+                  ),
                 ),
-                selectedDecoration: BoxDecoration(
-                  color: Colors.lightBlueAccent,
-                  shape: BoxShape.circle,
+                headerStyle: HeaderStyle(
+                  titleTextStyle: GoogleFonts.lato(
+                    textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  formatButtonTextStyle: GoogleFonts.lato(
+                    textStyle: TextStyle(color: Colors.white),
+                  ),
+                  formatButtonDecoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                ),
+                daysOfWeekStyle: DaysOfWeekStyle(
+                  weekdayStyle: TextStyle(
+                    color: widget.isDarkMode ? Colors.white : Colors.black,
+                  ),
+                  weekendStyle: TextStyle(
+                    color: widget.isDarkMode ? Colors.white : Colors.black,
+                  ),
                 ),
               ),
-              headerStyle: HeaderStyle(
-                titleTextStyle: GoogleFonts.lato(
-                  textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                formatButtonTextStyle: GoogleFonts.lato(
-                  textStyle: TextStyle(color: Colors.white),
-                ),
-                formatButtonDecoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(16.0),
-                ),
+              SizedBox(height: 20),
+              Wrap(
+                spacing: 8.0,
+                children: [
+                  _buildAnimatedButton('Previous week', () {
+                    setState(() {
+                      _focusedDay = DateTime.now().subtract(Duration(days: 7));
+                    });
+                  }),
+                  _buildAnimatedButton('Current week', () {
+                    setState(() {
+                      _focusedDay = DateTime.now();
+                    });
+                  }),
+                  _buildAnimatedButton('15 Days ago', () {
+                    setState(() {
+                      _focusedDay = DateTime.now().subtract(Duration(days: 15));
+                    });
+                  }),
+                  _buildAnimatedButton('12 Days ago', () {
+                    setState(() {
+                      _focusedDay = DateTime.now().subtract(Duration(days: 12));
+                    });
+                  }),
+                  _buildAnimatedButton('10 Days ago', () {
+                    setState(() {
+                      _focusedDay = DateTime.now().subtract(Duration(days: 10));
+                    });
+                  }),
+                  _buildAnimatedButton('5 Days ago', () {
+                    setState(() {
+                      _focusedDay = DateTime.now().subtract(Duration(days: 5));
+                    });
+                  }),
+                ],
               ),
-            ),
-            SizedBox(height: 20),
-            Wrap(
-              spacing: 8.0,
-              children: [
-                _buildAnimatedButton('Previous week', () {
-                  setState(() {
-                    _focusedDay = DateTime.now().subtract(Duration(days: 7));
-                  });
-                }),
-                _buildAnimatedButton('Current week', () {
-                  setState(() {
-                    _focusedDay = DateTime.now();
-                  });
-                }),
-                _buildAnimatedButton('15 Days ago', () {
-                  setState(() {
-                    _focusedDay = DateTime.now().subtract(Duration(days: 15));
-                  });
-                }),
-                _buildAnimatedButton('12 Days ago', () {
-                  setState(() {
-                    _focusedDay = DateTime.now().subtract(Duration(days: 12));
-                  });
-                }),
-                _buildAnimatedButton('10 Days ago', () {
-                  setState(() {
-                    _focusedDay = DateTime.now().subtract(Duration(days: 10));
-                  });
-                }),
-                _buildAnimatedButton('5 Days ago', () {
-                  setState(() {
-                    _focusedDay = DateTime.now().subtract(Duration(days: 5));
-                  });
-                }),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
