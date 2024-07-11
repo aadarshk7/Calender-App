f500e7bfad9f353fca1dd3af2e1683867d8ec08e
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-07-11 22:20:16 +0545

    Calender app

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class CalendarApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Calendar App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
